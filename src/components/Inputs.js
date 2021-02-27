285ca094d1b0c77651073ce938214f08e9b7e2b5
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-02-27 16:01:50 -0300

    Update inputs style

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,20 +2,31 @@ const Inputs = () => {
   return (
     <div className="inputs-container">
       {/* Concept */}
-      <label for="concept">Concepto:</label>
-      <input type="text" id="concept" name="concept" required minlength="1" maxlength="99" size="10"></input>
-      {/* Total amount */}
-      <label for="amount">Monto:</label>
-      <input type="number" id="amount" name="amount" required minlength="1" maxlength="99" size="10"></input>
+      <div className="input-group">
+        <label for="concept">Concepto</label>
+        <input type="text" id="concept" name="concept" required minlength="1" maxlength="99" size="10"></input>
+      </div>
+      {/* Amount */}
+      <div className="input-group">
+        <label for="amount">Monto</label>
+        <input type="number" id="amount" name="amount" required minlength="1" maxlength="99" size="10"></input>
+      </div>
       {/* Date */}
-      <label for="date">Fecha:</label>
-      <input type="date" id="date" name="date" required minlength="1" maxlength="99" size="10"></input>
-      {/* Income */}
-      <label for="income">Ingreso</label>
-      <input type="radio" id="income" name="input-type" value="income"></input>
-      {/* Expense */}
-      <label for="expense">Egreso</label>
-      <input type="radio" id="expense" name="input-type" value="expense"></input>
+      <div className="input-group">
+        <label for="date">Fecha</label>
+        <input type="date" id="date" name="date" required minlength="1" maxlength="99" size="10"></input>
+      </div>
+      {/* Income/Expense */}
+      <div className="input-group">
+        <label for="input-type">Tipo</label>
+        <select name="input-type" id="input-type">
+          <option value="income">Ingreso</option>
+          <option value="expense">Egreso</option>
+        </select>
+      </div>
+      <div className="input-group">
+        <button id="add-button">Agregar</button>
+      </div>
     </div>
   );
 }
