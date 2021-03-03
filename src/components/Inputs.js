dbe3213840b5fcf8187ca00033399f01c1f6765e
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-03 02:22:05 -0300

    Fix bug on form element (select)

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -5,36 +5,36 @@ const Inputs = ({income, setIncome, expense, setExpense}) => {
   const [concept, setConcept] = useState("")
   const [amount, setAmount] = useState(0)
   const [date, setDate] = useState("")
-  const [type, setType] = useState("income")
+  const [operation, setOperation] = useState("")
   console.log(expense)
   console.log(income)
   console.log(`Income length outside handleSubmit: ${income.length}`)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (income.length >= 10) {
-      console.log("Income array is equal to 10")
-      income.shift()
-    }
-    if (expense.length >= 10) {
-      console.log("Expense array is equal to 10")
-      expense.shift()
-    }
     // This will create a new object with the properties that are passed in from the input values
-    if (type === "income") {
+    if (operation === "income") {
       const newIncome = {
         concept: concept,
         amount: amount,
         date: date,
-        type: type
+        operation: operation
+      }
+      if (income.length === 10) {
+        console.log("Income array is equal to 10")
+        income.shift()
       }
       setIncome([...income, newIncome])
-    } else if((type === "expense")) {
+    } else if((operation === "expense")) {
       const newExpense = {
         concept: concept,
         amount: amount,
         date: date,
-        type: type
+        operation: operation
+      }
+      if (expense.length === 10) {
+        console.log("Expense array is equal to 10")
+        expense.shift()
       }
       setExpense([...expense, newExpense])
     } else {
@@ -44,12 +44,12 @@ const Inputs = ({income, setIncome, expense, setExpense}) => {
     setConcept("")
     setAmount("")
     setDate("")
-    setType("income")
+    setOperation(operation)
     // Console logs
     // console.log(`Concepto: ${concept}`)
     // console.log(`Monto: ${amount}`)
     // console.log(`Fecha: ${date}`)
-    // console.log(`Tipo: ${type}`)
+    // console.log(`Tipo: ${operation}`)
     // console.log(income[0].amount)
     console.log(`Income length inside handleSubmit: ${income.length}`)
   }
@@ -73,8 +73,9 @@ const Inputs = ({income, setIncome, expense, setExpense}) => {
       </div>
       {/* Income/Expense */}
       <div className="input-group">
-        <label htmlFor="input-type">Tipo</label>
-        <select name="input-type" id="input-type" onChange={e => setType(e.target.value)}>
+        <label htmlFor="operation">Tipo</label>
+        <select name="operation" id="operation" onChange={e => setOperation(e.target.value)}>
+          <option value="select">Seleccionar</option>
           <option value="income">Ingreso</option>
           <option value="expense">Egreso</option>
         </select>
