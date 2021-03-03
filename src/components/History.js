10a9359c723c9af36760d7b24f77bc3a21f5c6e4
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-03 01:12:18 -0300

    Add unique keys to childs

--- src/components/History.js
+++ src/components/History.js
@@ -3,8 +3,8 @@ const History = ({income, expense, index}) => {
     <div className="history-container">
       <div className="income-history">
         <h2>Ingresos</h2>
-        {income.map((item) => (
-          <div className="income-item" key={index}>
+        {income.map(item => (
+          <div className="income-item" key={ Math.random().toString(36).substr(2, 9) }>
             <p>${ item.amount }</p>
             <p>{ item.concept }</p>
             <p>{ item.date }</p>
@@ -15,8 +15,8 @@ const History = ({income, expense, index}) => {
       </div>
       <div className="expenses-history">
         <h2>Egresos</h2>
-        {expense.map((item) => (
-          <div className="expense-item" key={index}>
+        {expense.map(item => (
+          <div className="expense-item" key={ Math.random().toString(36).substr(2, 9) }>
             <p>${ item.amount }</p>
             <p>{ item.concept }</p>
             <p>{ item.date }</p>
